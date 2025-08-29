b9eb917dfa8f7cf5c304c4db5120b7e0a833bf88
Kaegii <camilosaudgasparinoo@gmail.com>
2025-08-29 19:05:45 -0300

    Add files via upload

---
+++ utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+// Converte uma string para minúsculas
+void para_minusculo(char *str);
+
+// Remove caracteres de pontuação e espaços de uma string
+void limpar_string(char *str);
+
+#endif
