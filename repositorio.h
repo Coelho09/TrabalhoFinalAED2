e0d6ff52e68a0e28ed7470ac5ae5897af35666a0
Kaegii <camilosaudgasparinoo@gmail.com>
2025-08-30 19:32:36 -0300

    repositorio.h

--- repositorio.h
+++ repositorio.h
@@ -1,28 +1,21 @@
 #ifndef REPOSITORIO_H
 #define REPOSITORIO_H
 
-
 #define MAX_LEN_PALAVRA 100
 #define MAX_MUSICA_NOME 100
 #define MAX_COMPOSITOR_NOME 100
 #define MAX_ESTROFE 101
 
-typedef struct musicaDados
-{
+typedef struct musicaDados {
     char nomeMus[MAX_MUSICA_NOME];
     char compo[MAX_COMPOSITOR_NOME];
     char estrofe[MAX_ESTROFE];
-    int frequencia;
-
 } musicaDados;
 
-typedef struct palavra_busca
-{
+typedef struct palavra_busca {
     char palavra[MAX_LEN_PALAVRA];
     musicaDados mData;
     int freqTotal;
 } palavra_busca;
 
-
-
-#endif 
+#endif
