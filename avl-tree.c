f4e438ac7199d9bb48c1350172e5516e208aa87b
Kaegii <camilosaudgasparinoo@gmail.com>
2025-08-30 19:26:18 -0300

    Camilo 2.0

--- avl-tree.c
+++ avl-tree.c
@@ -2,255 +2,115 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Funções de utilidade da AVL (altura, fator de balanceamento, rotações)
-int height( NoAVL *N)
-{
-    if (N == NULL)
-        return 0;
+int height(NoAVL *N) {
+    if (N == NULL) return 0;
     return N->altura;
 }
 
-
-int max(int a, int b)
-{
-    return (a > b)? a : b;
+int max(int a, int b) {
+    return (a > b) ? a : b;
 }
 
-
-
-NoAVL* newNodeAVL(palavra_busca *nova_entrada)
-{
-    NoAVL* node = (NoAVL*)
-                        malloc(sizeof(NoAVL));
+NoAVL* newNodeAVL(palavra_busca *nova_entrada) {
+    NoAVL* node = (NoAVL*) malloc(sizeof(NoAVL));
     node->entrada  = *nova_entrada;
     node->esquerda   = NULL;
     node->direita  = NULL;
-    node->altura = 1;  // novo no passa a ser folha
+    node->altura = 1;
     return(node);
 }
 
-
-
-NoAVL *rotacao_direita(NoAVL *y)
-{
+NoAVL *rotacao_direita(NoAVL *y) {
     NoAVL *x = y->esquerda;
     NoAVL *T2 = x->direita;
-
-    // rotacao
     x->direita = y;
     y->esquerda = T2;
-
-    // atualiza as alturas
-    y->altura = max(height(y->esquerda),
-                    height(y->direita)) + 1;
-    x->altura = max(height(x->esquerda),
-                    height(x->direita)) + 1;
-
-    
+    y->altura = max(height(y->esquerda), height(y->direita)) + 1;
+    x->altura = max(height(x->esquerda), height(x->direita)) + 1;
     return x;
 }
-// Implemente as rotações
 
-NoAVL* rotacao_esquerda(NoAVL* x){
+NoAVL* rotacao_esquerda(NoAVL* x) {
     NoAVL *y = x->direita;
     NoAVL *T2 = y->esquerda;
-
-    // rotacao
     y->esquerda = x;
     x->direita = T2;
-
-    // atualiza as alturas
-    x->altura = max(height(x->esquerda),   
-                    height(x->direita)) + 1;
-    y->altura = max(height(y->esquerda),
-                    height(y->direita)) + 1;
-
-    
+    x->altura = max(height(x->esquerda), height(x->direita)) + 1;
+    y->altura = max(height(y->esquerda), height(y->direita)) + 1;
     return y;
 }
 
 int fator_balanceamento(NoAVL* no) {
-    return no ? height(no->esquerda) - height(no->direita) : 0;
+    if (no == NULL) return 0;
+    return height(no->esquerda) - height(no->direita);
 }
 
-NoAVL* inserir_na_avl(NoAVL* node, palavra_busca *nova_entrada)
-{
-    NoAVL * insert_node = newNodeAVL(nova_entrada);
-    
-    
-    if (node == NULL)
-        return insert_node;
+NoAVL* inserir_na_avl(NoAVL* node, palavra_busca *nova_entrada) {
+    if (node == NULL) return newNodeAVL(nova_entrada);
+    int cmp = strcmp(nova_entrada->palavra, node->entrada.palavra);
+    if (cmp < 0) node->esquerda  = inserir_na_avl(node->esquerda, nova_entrada);
+    else if (cmp > 0) node->direita = inserir_na_avl(node->direita, nova_entrada);
+    else return node;
 
-    if(strcmp(node->entrada.palavra, insert_node->entrada.palavra) == 0)
-        if(node->entrada.mData.frequencia < insert_node->entrada.mData.frequencia)
-        {
-            node->entrada = insert_node->entrada;
-        }
-    else if (strcmp(node->entrada.palavra, insert_node->entrada.palavra) >0)
-        node->esquerda  = inserir_na_avl(node->esquerda, &insert_node->entrada);
-    else if (strcmp(node->entrada.palavra, insert_node->entrada.palavra)<0)
-        node->direita = inserir_na_avl(node->direita, &insert_node->entrada);
-    else 
-        return node;
-
-    // Atualiza a altura do no acestral
-    node->altura = 1 + max(height(node->esquerda),
-                        height(node->direita));
-
-    // verifica o fator de balanceamento do no ancestral!
+    node->altura = 1 + max(height(node->esquerda), height(node->direita));
     int balance = fator_balanceamento(node);
 
-    // Casos de desbalanceamento
-
-    // Esquerda Esquerda   //key < node->left->key
-    if (balance > 1 && strcmp(insert_node->entrada.palavra, node->esquerda->entrada.palavra) < 0)
-        return rotacao_direita(node);
-
-    // Direita Direita  key > node->right->key
-    if (balance < -1 && strcmp(insert_node->entrada.palavra, node->direita->entrada.palavra) > 0)
-        return rotacao_esquerda(node);
-
-    // Esquerda Direita key > node->left->key
-    if (balance > 1 && strcmp(insert_node->entrada.palavra, node->esquerda->entrada.palavra) > 0)
-    {
+    if (balance > 1 && strcmp(nova_entrada->palavra, node->esquerda->entrada.palavra) < 0) return rotacao_direita(node);
+    if (balance < -1 && strcmp(nova_entrada->palavra, node->direita->entrada.palavra) > 0) return rotacao_esquerda(node);
+    if (balance > 1 && strcmp(nova_entrada->palavra, node->esquerda->entrada.palavra) > 0) {
         node->esquerda =  rotacao_esquerda(node->esquerda);
         return rotacao_direita(node);
     }
-
-    // Direita Esquerda //key < node->right->key
-    if (balance < -1 &&  strcmp(insert_node->entrada.palavra, node->direita->entrada.palavra))
-    {
+    if (balance < -1 && strcmp(nova_entrada->palavra, node->direita->entrada.palavra) < 0) {
         node->direita = rotacao_direita(node->direita);
         return rotacao_esquerda(node);
     }
-
-    //retorna o nó
     return node;
 }
 
+NoAVL* inserir_na_avl_freq(NoAVL* node, palavra_busca *nova_entrada) {
+    if (node == NULL) return newNodeAVL(nova_entrada);
+    if (nova_entrada->freqTotal < node->entrada.freqTotal) node->esquerda  = inserir_na_avl_freq(node->esquerda, nova_entrada);
+    else if (nova_entrada->freqTotal >= node->entrada.freqTotal) node->direita = inserir_na_avl_freq(node->direita, nova_entrada); // >= para inserir duplicados
+    else return node;
 
-NoAVL* inserir_na_avl_freq(NoAVL* node, palavra_busca *nova_entrada)
-{
-    NoAVL * insert_node = newNodeAVL(nova_entrada);
-    
-    
-    if (node == NULL)
-        return insert_node;
-
-    if(node->entrada.freqTotal == insert_node->entrada.freqTotal)
-        if(node->entrada.mData.frequencia < insert_node->entrada.mData.frequencia)
-        {
-            node->entrada = insert_node->entrada;
-        }
-    else if (node->entrada.freqTotal > insert_node->entrada.freqTotal)
-        node->esquerda  = inserir_na_avl_freq(node->esquerda, &insert_node->entrada);
-    else if (node->entrada.freqTotal < insert_node->entrada.freqTotal)
-        node->direita = inserir_na_avl_freq(node->direita, &insert_node->entrada);
-    else 
-        return node;
-
-    // Atualiza a altura do no acestral
-    node->altura = 1 + max(height(node->esquerda),
-                        height(node->direita));
-
-    // verifica o fator de balanceamento do no ancestral!
+    node->altura = 1 + max(height(node->esquerda), height(node->direita));
     int balance = fator_balanceamento(node);
 
-    // Casos de desbalanceamento
-
-    // Esquerda Esquerda   //key < node->left->key
-    if (balance > 1 && strcmp(insert_node->entrada.palavra, node->esquerda->entrada.palavra) < 0)
-        return rotacao_direita(node);
-
-    // Direita Direita  key > node->right->key
-    if (balance < -1 && strcmp(insert_node->entrada.palavra, node->direita->entrada.palavra) > 0)
-        return rotacao_esquerda(node);
-
-    // Esquerda Direita key > node->left->key
-    if (balance > 1 && strcmp(insert_node->entrada.palavra, node->esquerda->entrada.palavra) > 0)
-    {
-        node->esquerda =  rotacao_esquerda(node->esquerda);
+    if (balance > 1 && nova_entrada->freqTotal < node->esquerda->entrada.freqTotal) return rotacao_direita(node);
+    if (balance < -1 && nova_entrada->freqTotal >= node->direita->entrada.freqTotal) return rotacao_esquerda(node);
+    if (balance > 1 && nova_entrada->freqTotal >= node->esquerda->entrada.freqTotal) {
+        node->esquerda = rotacao_esquerda(node->esquerda);
         return rotacao_direita(node);
     }
-
-    // Direita Esquerda //key < node->right->key
-    if (balance < -1 &&  strcmp(insert_node->entrada.palavra, node->direita->entrada.palavra))
-    {
+    if (balance < -1 && nova_entrada->freqTotal < node->direita->entrada.freqTotal) {
         node->direita = rotacao_direita(node->direita);
         return rotacao_esquerda(node);
     }
-
-    //retorna o nó
     return node;
 }
-
-// A busca na arvore AVL é feita da mesma forma que a busca na arvore BST
-// palavra_busca* buscar_na_avl(NoAVL* no,char* palavra) {
-    
-//     if (no == NULL || strcmp(no->entrada.palavra, palavra ) == 0)
-//         return no;
-
-    
-//     if (strcmp(no->entrada.palavra,palavra) < 0)
-//         return buscar_na_ab(no->direita, palavra);
-
-    
-//     return buscar_na_ab(no->esquerda, palavra);
-// }
-
 
 palavra_busca* buscar_na_avl(NoAVL* no, char* palavra) {
-    
-
-    
-      NoAVL* curr = no;
-    
-    while (curr != NULL) {
-        
-        
-        if (strcmp(curr->entrada.palavra, palavra) == 0)
-            return &(curr->entrada);
-            
-         //curr->data < x
-        else if (strcmp(curr->entrada.palavra, palavra) < 0) 
-            curr = curr->direita;
-            
-        // Search in left subtree
-        else
-            curr = curr->esquerda;
+    NoAVL* atual = no;
+    while (atual != NULL) {
+        int cmp = strcmp(palavra, atual->entrada.palavra);
+        if (cmp == 0) return &(atual->entrada);
+        else if (cmp < 0) atual = atual->esquerda;
+        else atual = atual->direita;
     }
-    
-    
     return NULL;
 }
-
 
 palavra_busca* buscar_na_avl_por_frequencia(NoAVL* no, int freq) {
-    
-
-    
-      NoAVL* curr = no;
-    
-    while (curr != NULL) {
-        
-        
-        if (curr->entrada.freqTotal ==  freq)
-            return &(curr->entrada);
-            
-         //curr->data < x
-        else if (curr->entrada.freqTotal < freq) 
-            curr = curr->direita;
-            
-        // Search in left subtree
-        else
-            curr = curr->esquerda;
+    NoAVL* atual = no;
+    while (atual != NULL) {
+        if (freq == atual->entrada.freqTotal) return &(atual->entrada);
+        else if (freq < atual->entrada.freqTotal) atual = atual->esquerda;
+        else atual = atual->direita;
     }
-    
-    
+
     return NULL;
 }
-
-
 
 void liberar_avl(NoAVL* no) {
     if (no != NULL) {
