8083b59339ddd38b5a95bffcb2ae55d275e148d6
Kaegii <camilosaudgasparinoo@gmail.com>
2025-08-30 19:29:43 -0300

    Olha

--- binary_search.h
+++ binary_search.h
@@ -2,7 +2,6 @@
 #define BINARY_SEARCH_H
 #include "repositorio.h"
 
-
 typedef struct {
     palavra_busca* vetor;
     int tamanho;
@@ -10,11 +9,9 @@ typedef struct {
 } RepositorioVetor;
 
 void inicializa_vetor(RepositorioVetor* rv);
-void inserir_no_vetor(RepositorioVetor* rv, palavra_busca * nova_entrada);
+void inserir_no_vetor(RepositorioVetor* rv, palavra_busca* nova_entrada);
 palavra_busca* buscar_no_vetor(RepositorioVetor* rv, char* palavra);
-void ordenar_vetor_alfabeticamente(RepositorioVetor* rv);
 void liberar_vetor(RepositorioVetor* rv);
-void shell_sort_vetor(RepositorioVetor  *rv, int vec_size);
-int buscar_no_vetor_indice(RepositorioVetor* rv, char* palavra);
+void shell_sort_vetor(RepositorioVetor *rv, int vec_size);
 
 #endif
