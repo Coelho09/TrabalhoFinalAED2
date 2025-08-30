9af34175fb310acdde690d7b8cf95ac7b2e7979c
Kaegii <camilosaudgasparinoo@gmail.com>
2025-08-30 19:33:27 -0300

    utils.h

--- utils.h
+++ utils.h
@@ -1,11 +1,7 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-// Converte uma string para minúsculas
 void para_minusculo(char *str);
-
-// Remove caracteres de pontuação e espaços de uma string
-void limpar_string(char *str);
 void trim(char* str);
 
 #endif
