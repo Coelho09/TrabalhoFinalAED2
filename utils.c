c5660e738a72df8bac9ae98464644008acf51a42
Kaegii <camilosaudgasparinoo@gmail.com>
2025-08-30 19:33:03 -0300

    MEU DEUS

--- utils.c
+++ utils.c
@@ -4,65 +4,23 @@
 
 void para_minusculo(char *str) {
     for (int i = 0; str[i]; i++) {
-        str[i] = tolower(str[i]);
-    }   
-}
-
-void remove_acento(char *str) {
-    for (int i = 0; str[i]; i++){
-        switch(str[i]) {
-            case 'á': case 'à': case 'ã': case 'â': case 'ä': str[i] = 'a'; break;
-            case 'Á': case 'À': case 'Ã': case 'Â': case 'Ä': str[i] = 'A'; break;
-            case 'é': case 'è': case 'ê': case 'ë': str[i] = 'e'; break;
-            case 'É': case 'È': case 'Ê': case 'Ë': str[i] = 'E'; break;
-            case 'í': case 'ì': case 'î': case 'ï': str[i] = 'i'; break;
-            case 'Í': case 'Ì': case 'Î': case 'Ï': str[i] = 'I'; break;
-            case 'ó': case 'ò': case 'õ': case 'ô': case 'ö': str[i] = 'o'; break;
-            case 'Ó': case 'Ò': case 'Õ': case 'Ô': case 'Ö': str[i] = 'O'; break;
-            case 'ú': case 'ù': case 'û': case 'ü': str[i] = 'u'; break;
-            case 'Ú': case 'Ù': case 'Û': case 'Ü': str[i] = 'U'; break;
-            case 'ç': str[i] = 'c'; break;
-            case 'Ç': str[i] = 'C'; break;
-        }
+        str[i] = tolower((unsigned char)str[i]);
     }
 }
-
-// "limpa" o caractere atual da string
-void limpar_string(char *str) {
-    int i, j = 0;
-
-    remove_acento(str);
-
-    for (i = 0; str[i] != '\0'; i++){
-        char c = str[i];
-        if (isalnum((unsigned char)c) || c == ' '){
-            str[j++] = c;
-        }
-    }
-    str[j] = '\0';
-
-    para_minusculo(str);
-}
-
-
 
 void trim(char* str) {
     if (str == NULL) {
         return;
     }
-
     int inicio = 0;
     int fim = strlen(str) - 1;
-
     while (fim >= 0 && isspace((unsigned char)str[fim])) {
         fim--;
     }
     str[fim + 1] = '\0';
-
     while (isspace((unsigned char)str[inicio])) {
         inicio++;
     }
-
     if (inicio > 0) {
         memmove(str, str + inicio, fim - inicio + 2);
     }
